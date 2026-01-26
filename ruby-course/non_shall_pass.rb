96ac0131615dd4d702dbfdf00a3e2086f0bfcf5f
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-01-27 02:03:47 +0530

    Fix typo in welcome_message method and refactor user selection handling for improved clarity

--- ruby-course/non_shall_pass.rb
+++ ruby-course/non_shall_pass.rb
@@ -4,7 +4,7 @@ PASSWORD="Pakaya@123"
 PASSWORD_VAULT = {aws: {username: "Ubetatta", password: "Bahukpn"}}
 
 # welcome messages
-def welcom_essage 
+def welcome_message 
   puts "Welcome to Non Shall Pass - A Password Manager"
 end
 
@@ -45,49 +45,48 @@ def menu_options
   puts "1. Add service credentials"
   puts "2. Retrieve and existing service credentials"
   puts "3. Exit"
-  user_selection = gets.chomp
 end
 
-else # if password is wrong
-    puts "Invalid Password"
+def get_user_menu_selection
+  gets.chomp
+end
+
+def handle_user_selection(user_selection)
+  # performing user selection according to the input
+  case user_selection
+  when "1"
+    # create a new service 
+    puts "This will create new service credentials"
+    print "Enter the name of the service: "
+    new_service=gets.chomp
+    PASSWORD_VAULT[new_service]={}
+    p PASSWORD_VAULT
+
+    # gets username for the created service
+    print "Enter the username for new service: "
+    new_service_user_name=gets.chomp 
+    PASSWORD_VAULT[new_service]["username"]=new_service_user_name
+    p PASSWORD_VAULT
+
+    # gets password for the created service
+    print "Enter the password for new service: "
+    new_service_password=gets.chomp
+    PASSWORD_VAULT[new_service]["password"]=new_service_password
+    p PASSWORD_VAULT
+
+  when "2"
+    puts "This will retrieve existing service credentials"
+    print "Please enter the name of the service you wish to access credentials for: "
+    requested_service_name=gets.chomp
+    credentials=PASSWORD_VAULT[requested_service_name.to_sym]
+    puts "Here are the credentials for #{requested_service_name}: "
+    credentials.each do |key, val|
+      puts "#{key}, #{val}."
+    end
+  when "3"
+    puts "Exiting the program. Goodbye motherfucker!"
+  else 
+    puts "Invalid Input. Please put the option number only.(Ex: 1)"
     exit
-end
-
-# performing user selection according to the input
-case user_selection
-when "1"
-  
-  # create a new service 
-  puts "This will create new service credentials"
-  print "Enter the name of the service: "
-  new_service=gets.chomp
-  PASSWORD_VAULT[new_service]={}
-  p PASSWORD_VAULT
-
-  # gets username for the created service
-  print "Enter the username for new service: "
-  new_service_user_name=gets.chomp 
-  PASSWORD_VAULT[new_service]["username"]=new_service_user_name
-  p PASSWORD_VAULT
-
-  # gets password for the created service
-  print "Enter the password for new service: "
-  new_service_password=gets.chomp
-  PASSWORD_VAULT[new_service]["password"]=new_service_password
-  p PASSWORD_VAULT
-
-when "2"
-  puts "This will retrieve existing service credentials"
-  print "Please enter the name of the service you wish to access credentials for: "
-  requested_service_name=gets.chomp
-  credentials=PASSWORD_VAULT[requested_service_name.to_sym]
-  puts "Here are the credentials for #{requested_service_name}: "
-  credentials.each do |key, val|
-    puts "#{key}, #{val}."
   end
-when "3"
-  puts "Exiting the program. Goodbye motherfucker!"
-else 
-  puts "Invalid Input. Please put the option number only.(Ex: 1)"
-  exit
 end
